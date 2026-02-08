c97f483ff6210a1eb23eaf77c0898d3d206f3c5b
amanbolat <1@amanbolat.com>
2026-02-08 21:27:41 +0100

    fix(scripts): disable nounset for sdkman init
    - Temporarily disable `nounset` before sourcing `sdkman-init.sh`
    - Re-enable `nounset` after sourcing the script
    - This addresses potential compatibility issues between `nounset` and the `sdkman` initialization script

--- scripts/install/40-java.sh
+++ scripts/install/40-java.sh
@@ -11,6 +11,9 @@ curl -s "https://get.sdkman.io?rcupdate=false" | bash
 append_if_missing 'source "$HOME/.sdkman/bin/sdkman-init.sh"' "$HOME/.bashrc"
 append_if_missing 'source "$HOME/.sdkman/bin/sdkman-init.sh"' "$HOME/.zshrc"
 
+# sdkman init script is not fully compatible with nounset; scope-disable it.
+set +u
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java 21.0.9-tem
 sdk install gradle
+set -u
